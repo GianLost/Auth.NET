05d539ccfcfea38194a38fe61e967c6085d35901
GianLost <gianluca19993m@gmail.com>
2024-11-18 18:02:24 -0300

    refatoração de estilos do layout par ao projeto teste da solução Auth.NET

--- src/Auth.NET.Libs/Helpers/Messages/ErrorMessages.cs
+++ src/Auth.NET.Libs/Helpers/Messages/ErrorMessages.cs
@@ -1,5 +1,5 @@
 ﻿namespace Auth.NET.Libs.Helpers.Messages;
 public class ErrorMessages
 {
-
+    public const string MsgPasswordRegex = "A senha deve conter no mínimo 8 caracteres, letras maiúsculas, minúsculas, números e símbolos !";
 }
---
+++ src/AuthSample/Controllers/Users/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSample.Controllers.Users;
+
+public class UserController : Controller
+{
+    public IActionResult Index() => View();
+    public IActionResult Register() => View();
+}
